64311069288bae5a9aeea2dbc6b7faac9ea0f818
Tony Robertson <tonyr729@gmail.com>
2018-07-10 16:33:22 -0600

    Add titles and urls for games data

--- games_data.js
+++ games_data.js
@@ -3,7 +3,88 @@ const games = [
     title: "Tomb Raider",
     url: "https://www.tombraider.com/en-us",
     genre: "Action"
-  }
+  },
+]
+
+
+const rpgTitles = [ 
+  'Fallout 76',
+  'Assassin\'s Creed Odyssey',
+  'Dragon Quest XI: Echoes of an Elusive Age',
+  'The Bard’s Tale IV',
+  'Immortal: Unchained',
+  'Deep Sky Derelicts',
+  'Underworld Ascendant',
+  'Slasher’s Keep',
+  'Adventure Time: Pirates of the Enchiridion',
+  'The Quest Giver'
+]
+
+const actionTitles = [ 
+  'Shadow of the Tomb Raider',
+  'Just Cause 4',
+  'Hitman 2',
+  'Monster Hunter World',
+  'Biomutant',
+  'Code Vein',
+  'MechWarrior 5: Mercenaries',
+  'God\'s Trigger',
+  'Zone of the Enders: The 2nd Runner ',
+  'Mugsters'
+]
+
+const shooterTitles = [ 
+  'Battlefield V',
+  'Call of Duty: Black Ops 4',
+  'Destiny 2: Forsaken Expansion',
+  'Mothergunship',
+  'Unreal Tournament',
+  'Strange Brigade',
+  'Overkill\'s The Walking Dead',
+  'Consortium: The Tower ',
+  'Superhot: Mind Control Delete',
+  'Deep Rock Galactic',
+  'Left Alive'
+]
+
+const rpgUrls = [ 
+  'https://www.pcgamer.com/fallout-76-is-a-multiplayer-softcore-survival-game-about-building-settlements/',
+  'https://www.pcgamer.com/assassins-creed-odyssey-pc-release-date-trailer-system-requirements/',
+  'https://www.pcgamer.com/dragon-quest-11-confirmed-for-pc-release-date-set/',
+  'https://bardstale.inxile-entertainment.com/',
+  'http://store.steampowered.com/app/369440/Immortal_Unchained/',
+  'http://www.snowhoundgames.com/deep-sky-derelicts/',
+  'https://otherside-e.com/wp/games/underworld-ascendant/',
+  'http://www.slasherskeep.info/',
+  'https://www.outrightgames.com/adventure-time-pirates-of-the-enchiridion/',
+  'http://thinkrestless.com/the-quest-giver/'
+]
+
+const actionUrls = [ 
+  'https://store.steampowered.com/app/750920/Shadow_of_the_Tomb_Raider/',
+  'https://store.steampowered.com/app/517630/Just_Cause_4/',
+  'https://store.steampowered.com/app/863550/HITMAN2/',
+  'http://www.monsterhunterworld.com/',
+  'http://biomutant.com/',
+  'https://www.xbox.com/en-US/games/code-vein',
+  'https://mw5mercs.com/',
+  'https://godstriggergame.com/',
+  'https://store.steampowered.com/app/650510/ZONE_OF_THE_ENDERS_THE_2nd_RUNNER__MRS/',
+  'http://store.steampowered.com/app/712180/Mugsters/'
+]
+
+const shooterUrls = [ 
+  'https://www.pcgamer.com/everything-you-need-to-know-about-battlefield-5/',
+  'https://www.pcgamer.com/black-ops-4-borrows-from-overwatch-and-siege-to-finally-modernize-call-of-duty/',
+  'https://www.pcgamer.com/destiny-2-forsaken-bringing-a-new-multiplayer-mode-weapon-slot-changes-9-new-supers/',
+  'http://www.mothergunship.com/',
+  'https://www.epicgames.com/unrealtournament/',
+  'http://strangebrigade.com/en/',
+  'https://www.overkillsthewalkingdead.com/',
+  'http://thetowergame.com/',
+  'https://superhotgame.com/',
+  'https://www.deeprockgalactic.com/',
+  'https://www.left-alive.com/en/agegate'
 ]
 
 module.exports = games;
