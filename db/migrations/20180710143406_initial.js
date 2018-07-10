e06e1c410844d4e8057aa623a058a72278ea594b
Tony Robertson <tonyr729@gmail.com>
2018-07-10 14:40:33 -0600

    Create knex file with table migrations

---
+++ db/migrations/20180710143406_initial.js
@@ -0,0 +1,30 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('games', function(table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('url');
+      table.string('genre');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('pictures', function(table) {
+      table.increments('id').primary();
+      table.string('url');
+      table.integer('game_id').unsigned()
+      table.foreign('game_id')
+        .references('games.id');
+
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('games'),
+    knex.schema.dropTable('pictures')
+  ]);
+};
