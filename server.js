f6b8195f4a2ab652ead40ea41c4217cc07ca9dd8
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-11 13:13:01 -0600

    Add get endpoint for games table

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Chat Box';
+app.locals.title = 'BYOB';
 
-app.get('/', (request, response) => {
-  response.send('Oh hey Chat Box');
+app.use(bodyParser.json())
+
+app.get('/api/v1/games', (request, response) => {
+  database('games').select()
+    .then(games => {
+      response.status(200).json(games)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
 });
 
 app.listen(app.get('port'), () => {
