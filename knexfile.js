40f0697f1ba40b18e7d135cf5c1c6b4f2b057408
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-13 12:16:44 -0600

    Lint all files

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/byob_test',
     migrations: {
-      directory:'./db/migrations'
+      directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/test'
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
-const checkAuth = require('./checkAuth/checkAuth')
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const checkAuth = require('./checkAuth/checkAuth');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOB';
@@ -12,174 +12,204 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-console.log(environment)
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.post('/', (request, response) => {
   const { email, appName } = request.body;
   
   if ( email && appName) {
-    const payload = { email, appName }
+    const payload = { email, appName };
 
     try {
       const token = jwt.sign(payload, process.env.SECRET_KEY);
   
-      response.status(201).json({ token })
+      response.status(201).json({ token });
     } catch (error) {
-      response.status(500).json({error})
+      response.status(500).json({error});
     }
   } else {
-    response.status(500).json({error: 'Invalid keys within request body.'})
+    response.status(500).json({error: 'Invalid keys within request body.'});
   }
 });
 
 app.get('/api/v1/games', (request, response) => {
   database('games').select()
     .then(games => {
-      response.status(200).json(games)
+      response.status(200).json(games);
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
+      response.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/games/:id', (request, response) => {
-  const { id } = request.params
+  const { id } = request.params;
+
   database('games').where('id', id).select()
     .then(game => {
-      if(game.length) {
-        response.status(200).json(game)
+      if (game.length) {
+        response.status(200).json(game);
       } else {
         throw Error;
       }
     })
     .catch(error => {
-      response.status(500).json({ errorMessage: `Could not find game with id of ${id}`, error })
-    })
-})
+      response.status(500).json(
+        { errorMessage: `Could not find game with id of ${id}`, error }
+      );
+    });
+});
 
 app.get('/api/v1/pictures', (request, response) => {
   database('pictures').select()
     .then(pictures => {
-      response.status(200).json(pictures)
+      response.status(200).json(pictures);
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/pictures/:id', (request, response) => {
-  const {id} = request.params
+  const {id} = request.params;
+
   database('pictures').where('id', id).select()
     .then(picture => {
-      if(picture.length) {
-        response.status(200).json(picture)
+      if (picture.length) {
+        response.status(200).json(picture);
       } else {
         throw Error;
       }
     })
     .catch(error => {
-      response.status(500).json({ errorMessage: `Could not find picture with id of ${id}`, error })        
-    })
-})
+      response.status(500).json(
+        { errorMessage: `Could not find picture with id of ${id}`, error }
+      );        
+    });
+});
 
 app.post('/api/v1/games', checkAuth, (request, response) => {
-  const { game } = request.body
-  for(let requiredParameter of ['title', 'url', 'genre']) {
-    if(!game[requiredParameter]){
+  const { game } = request.body;
+
+  for (let requiredParameter of ['title', 'url', 'genre']) {
+    if (!game[requiredParameter]) {
       return response
         .status(422)
-        .send({ error: `Expected format {title: <String>, url: <String>, genre: <String>}. You're missing a ${requiredParameter} property.`})
+        .send(
+          { error: `Expected format 
+            {
+              title: <String>, 
+              url: <String>, 
+              genre: <String>
+            }
+            You're missing a ${requiredParameter} property.`
+          }
+        );
     } 
   }
   database('games').insert(game, 'id')
     .then(gameId => {
-      response.status(201).json({ id: gameId[0] })
+      response.status(201).json({ id: gameId[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.post('/api/v1/pictures', checkAuth, (request, response) => {
   const { picture } = request.body;
+
   for (let requiredParameter of ['url', 'game_id', 'gameName']) {
-    if(!picture[requiredParameter]) {
+    if (!picture[requiredParameter]) {
       return response
         .status(422)
-        .send({ error: `Expected format {url: <String>, game_id: <Integer>, gameName: <String>} You're missing a ${requiredParameter} property.`})
+        .send(
+          { error: `Expected format: 
+            {
+              url: <String>,
+              game_id: <Integer>,
+              gameName: <String>
+            } 
+            You're missing a ${requiredParameter} property.`
+          }
+        );
     }
   }
   database('games').where('id', picture.game_id).select()
     .then(game => {
-      if(game.length) {
+      if (game.length) {
         database('pictures').insert(picture, 'id')
           .then(pictureId => {
-            response.status(201).json({ id: pictureId[0] })
+            response.status(201).json({ id: pictureId[0] });
           })
-          .catch( error => {
-            throw Error
-          })
+          .catch(() => {
+            throw Error;
+          });
       } else {
-        response.status(500).json({ error: 'Could not find matching game for picture submitted' })
+        response.status(500).json(
+          { error: 'Could not find matching game for picture submitted' }
+        );
       }
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.patch('/api/v1/games/:id', checkAuth, (request, response) => {
-  const { game } = request.body
-  const { id } = request.params
+  const { game } = request.body;
+  const { id } = request.params;
+
   database('games').where('id', id).update(game, 'id')
     .then(gameId => {
-      response.status(202).json({ id: gameId[0] })
+      response.status(202).json({ id: gameId[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.patch('/api/v1/pictures/:id', checkAuth, (request, response) => {
-  const { picture } = request.body
-  const { id } = request.params
+  const { picture } = request.body;
+  const { id } = request.params;
+
   database('pictures').where('id', id).update(picture, 'id')
     .then(pictureId => {
-      response.status(202).json({ id: pictureId[0] })
+      response.status(202).json({ id: pictureId[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.delete('/api/v1/games/:id', checkAuth, (request, response) => {
   const { id } = request.params;
+
   database('pictures').where('game_id', id).del()
     .then(() => {
-      response.sendStatus(204)
+      response.sendStatus(204);
     })
     .then(() => {
       database('games').where('id', id).del()
         .then(() => response.sendStatus(204))
-        .catch(error => response.status(404).json({ Error: `Cannot find matching game id: ${id}`}))
+        .catch(() => response.status(404).json(
+          { Error: `Cannot find matching game id: ${id}`}
+        ));
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.delete('/api/v1/pictures/:id', checkAuth, (request, response) => {
   const { id } = request.params;
+
   database('pictures').where('id', id).del()
     .then(() => response.sendStatus(204))
-    .catch(error => response.status(404).json({ Error: `Cannot find matching id: ${id}`}))
-})
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+    .catch(() => response.status(404).json(
+      { Error: `Cannot find matching id: ${id}`}
+    ));
 });
 
 module.exports = app;
