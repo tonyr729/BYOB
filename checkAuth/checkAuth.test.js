b2198e98e7154a605db33cca7e50d398b2d44172
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-13 09:10:17 -0600

    Stub tests for checkAuth

--- checkAuth/checkAuth.test.js
+++ checkAuth/checkAuth.test.js
@@ -3,5 +3,25 @@ const chai = require('chai');
 const should = chai.should();
 
 describe('checkAuth', () => {
-  
+  it('should call next if the environment is test', () => {
+    let process = {
+      env: {
+        NODE_ENV: 'test'
+      }
+    }
+    checkAuth();
+    
+  })
+
+  it('should call next if the the request.body has a token that is authenticated', () => {
+
+  })
+
+  it('should send a response with a status of 401 if the token is not authorized', () => {
+
+  })
+
+  it('should send a response with a status of 403 if the token is not present', () => {
+
+  })
 })
